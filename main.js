dc174e3f67685dc3b6d0fd29e20050a0bc3bcf1f
Nickhichida <88297144+Nickhichida@users.noreply.github.com>
2021-10-27 22:14:27 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -9,50 +9,12 @@ boton.addEventListener("click", (e)=>{
 });
 
 let logo = document.querySelector(".container__logo");
-let buttonHome = document.querySelector(".button__home");
-let buttonSkills = document.querySelector(".button__skills");
-let buttonPorfolio = document.querySelector(".button__portfolio");
-let buttonContact = document.querySelector(".button__contact");
 
 
 logo.addEventListener("click", (e)=>{
     e.preventDefault();
     window.scroll({
         top: 0,
-        left: 0,
-        behavior: 'smooth'
-    });
-})
-buttonHome.addEventListener("click", (e)=>{
-    e.preventDefault();
-    window.scroll({
-        top: 30,
-        left: 0,
-        behavior: 'smooth'
-    });
-})
-
-buttonSkills.addEventListener("click", (e)=>{
-    e.preventDefault();
-    window.scroll({
-        top: 685,
-        left: 0,
-        behavior: 'smooth'
-    });
-})
-buttonPorfolio.addEventListener("click", (e)=>{
-    e.preventDefault();
-    window.scroll({
-        top: 1252,
-        left: 0,
-        behavior: 'smooth'
-    });
-
-})
-buttonContact.addEventListener("click", (e)=>{
-    e.preventDefault();
-    window.scroll({
-        top: 1325,
         left: 0,
         behavior: 'smooth'
     });
